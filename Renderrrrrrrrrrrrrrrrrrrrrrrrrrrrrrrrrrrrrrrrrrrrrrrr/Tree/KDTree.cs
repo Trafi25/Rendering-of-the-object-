4e751d35319415eea2dc6cbee023c40da2d24315
Trafi25 <45690086+Trafi25@users.noreply.github.com>
2021-04-21 20:07:14 +0300

    Nothing(

---
+++ Renderrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr/Tree/KDTree.cs
@@ -0,0 +1,31 @@
+﻿using Renderrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr.Structures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Renderrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr.Tree
+{
+
+
+    public class KdTree
+    {
+      
+
+        public KdTree(Triangle[] triangles) 
+        {
+            var Xmin = float.MaxValue;
+            var Ymin = float.MaxValue;
+            var Zmin = float.MaxValue;
+            var Xmax = float.MinValue;
+            var Ymax = float.MinValue;
+            var Zmax = float.MinValue;
+
+            
+
+           
+        }
+
+    }
+
+}
